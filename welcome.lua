568bad3219433acc243ed5a133d85bd36457566e
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-02-25 01:25:05 +1100

    Delete welcome.lua

--- welcome.lua
+++
@@ -1,7 +0,0 @@
-writefile('sound.mp3', game:HttpGet('https://raw.githubusercontent.com/GlobeReverse/Lua-Projects/main/welcome-mp3.ogg'))
-
-local audio = Instance.new("Sound", workspace)
-audio.SoundId = getcustomasset('sound.mp3')
-audio:play()
-
-delfile('sound.mp3')
