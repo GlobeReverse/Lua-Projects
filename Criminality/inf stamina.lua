44366d984802b1329f9caa21498633d447271e0b
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-03-06 21:33:25 +1100

    Update inf stamina.lua

--- Criminality/inf stamina.lua
+++ Criminality/inf stamina.lua
@@ -1,10 +1,10 @@
 -- cannot get the function from _G as ro exec dont work like that :(
-for i,v in pairs(getgc()) do 
-    if typeof(v) == "function" then 
-        local debuginfo = debug.getinfo(v)
+for index, value in pairs(getgc()) do 
+    if typeof(value) == "function" then 
+        local debuginfo = debug.getinfo(value)
 
         if (debuginfo.name == "S_Get") then 
-            hookfunction(v, function(...)
+            hookfunction(value, function(...)
                 return 100, 100
             end)
         end
