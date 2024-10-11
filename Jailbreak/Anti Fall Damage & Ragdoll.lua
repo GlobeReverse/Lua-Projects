300d54021059e69717030cadecc38cb89de021d7
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-10-11 21:51:42 +1100

    Update Anti Fall Damage & Ragdoll.lua

--- Jailbreak/Anti Fall Damage & Ragdoll.lua
+++ Jailbreak/Anti Fall Damage & Ragdoll.lua
@@ -1,19 +1,19 @@
 --// Services
-local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local ReplicatedStorage: ReplicatedStorage = game:GetService("ReplicatedStorage");
 
 --// Variables
-local fallModule = require(ReplicatedStorage.Game.Falling);
+local fallModule: any = require(ReplicatedStorage.Game.Falling);
 
-local converstion = {
+local converstion: { [string]: string } = {
     ["EnterVehicle"] = ("q" .. "" .. ("m1dpq1mw"):sub(2, 4):reverse():reverse():reverse() .. "" .. "" .. ("cv0z8hd4"):sub(5, 6):reverse():reverse():reverse() .. "" .. "fy"),
     ["ArrestPlayer"] = ("s" .. "" .. ("hfenz"):sub(4, 4):reverse() .. "" .. "" .. ("lp3idhr5"):sub(2, 4):reverse() .. "" .. "" .. ("frpwfhd"):sub(3, 3):reverse() .. "" .. "" .. ("h3k"):sub(2, 2):reverse() .. "" .. "b"),
     ["FallDamage"] = ("g" .. ("ptakw9b4"):sub(2, 4):reverse():reverse():reverse() .. "" .. "" .. ("t5hkoox"):sub(4, 6):reverse() .. "" .. "p")
 }
 
 --// Garbage Collector
-for _, value in pairs(getgc(true)) do 
+for _, value: any in pairs(getgc(true)) do 
     if (typeof(value) == "table") then 
-        local fireServer = rawget(value, "FireServer");
+        local fireServer: any = rawget(value, "FireServer");
 
         if fireServer then 
             fireTable = value;
@@ -22,13 +22,13 @@ for _, value in pairs(getgc(true)) do
 end
 
 --// Handling
-hookfunction(fallModule.StartRagdolling, function() 
+hookfunction(fallModule.StartRagdolling, function(): any 
     return;
 end)
 
 local FireServer = fireTable.FireServer;
 
-function fireTable.FireServer(self, key, ...)
+function fireTable.FireServer(self: any, key, ...): ...any
     if (key == converstion.FallDamage) then 
         return;
     end
