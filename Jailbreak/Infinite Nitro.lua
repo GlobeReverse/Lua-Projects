a50ef4d776f0a4ab25c855ba2c5456f48af890c0
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-10-11 18:37:58 +1100

    Create Infinite Nitro.lua

---
+++ Jailbreak/Infinite Nitro.lua
@@ -0,0 +1,11 @@
+for _, value: any in pairs(getgc(true)) do 
+    if (typeof(value) == "table") then 
+        local yi4308my = rawget(value, "yi4308my");
+
+        if yi4308my then 
+            while task.wait() do 
+                yi4308my(5400, 5400);
+            end
+        end
+    end
+end
