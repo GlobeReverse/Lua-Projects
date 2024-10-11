8fadaec658d0499b8cdebe012a710ef1b667b20f
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-10-11 21:53:25 +1100

    Update Gun Mods.lua

--- Jailbreak/Gun Mods.lua
+++ Jailbreak/Gun Mods.lua
@@ -2,11 +2,11 @@
 local ReplicatedStorage: ReplicatedStorage = game:GetService("ReplicatedStorage");
 
 --// Variables
-local gunModule = require(ReplicatedStorage.Game.Item.Gun);
+local gunModule: any = require(ReplicatedStorage.Game.Item.Gun);
 
 --// Handling
-local newHook; newHook = hookfunction(gunModule.new, function(v64, v65, v66)
-    local self = newHook(v64, v65, v66);
+local newHook: (any, any, any) -> any; newHook = hookfunction(gunModule.new, function(v64, v65, v66): any
+    local self: any = newHook(v64, v65, v66);
     
     --# Instant Reload
     rawset(self.Config, "ReloadTime", -1);
