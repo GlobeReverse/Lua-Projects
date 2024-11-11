53162bb2105fceb7d8bfb0491d2c35cdfd843c10
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-11-11 21:55:44 +1100

    Update Anti Fall Damage & Ragdoll.lua

--- Jailbreak/Anti Fall Damage & Ragdoll.lua
+++ Jailbreak/Anti Fall Damage & Ragdoll.lua
@@ -4,11 +4,7 @@ local ReplicatedStorage: ReplicatedStorage = game:GetService("ReplicatedStorage"
 --// Variables
 local fallModule: any = require(ReplicatedStorage.Game.Falling);
 
-local converstion: { [string]: string } = {
-    ["EnterVehicle"] = ("q" .. "" .. ("m1dpq1mw"):sub(2, 4):reverse():reverse():reverse() .. "" .. "" .. ("cv0z8hd4"):sub(5, 6):reverse():reverse():reverse() .. "" .. "fy"),
-    ["ArrestPlayer"] = ("s" .. "" .. ("hfenz"):sub(4, 4):reverse() .. "" .. "" .. ("lp3idhr5"):sub(2, 4):reverse() .. "" .. "" .. ("frpwfhd"):sub(3, 3):reverse() .. "" .. "" .. ("h3k"):sub(2, 2):reverse() .. "" .. "b"),
-    ["FallDamage"] = ("g" .. ("ptakw9b4"):sub(2, 4):reverse():reverse():reverse() .. "" .. "" .. ("t5hkoox"):sub(4, 6):reverse() .. "" .. "p")
-}
+local converstion = loadstring(game:HttpGet("https://raw.githubusercontent.com/GlobeReverse/Lua-Projects/refs/heads/main/Jailbreak/Converstions.lua"))();
 
 --// Garbage Collector
 for _, value: any in pairs(getgc(true)) do 
