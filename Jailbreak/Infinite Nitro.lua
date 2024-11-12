345b5f968d5e48e6b42acef1f47c01014bc53307
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-11-12 22:14:18 +1100

    Update Infinite Nitro.lua

--- Jailbreak/Infinite Nitro.lua
+++ Jailbreak/Infinite Nitro.lua
@@ -2,7 +2,7 @@ for _, value: any in pairs(getgc(true)) do
     if (typeof(value) == "table") then 
         local xf1lrqfq: any = rawget(value, "xf1lrqfq");
 
-        if yi4308my then 
+        if xf1lrqfq then 
             while task.wait() do 
                 xf1lrqfq(5400, 5400);
             end
