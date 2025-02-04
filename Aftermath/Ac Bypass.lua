d4ed8a0bc671ba8d47ae95a5faf8c26d4b783fd8
Globe <92859545+GlobeReverse@users.noreply.github.com>
2025-02-04 20:02:19 +1100

    Update Ac Bypass.lua

--- Aftermath/Ac Bypass.lua
+++ Aftermath/Ac Bypass.lua
@@ -1,3 +1,4 @@
+--# Shitty method not sure if its stable to use however it will stay like this since I don't want to leak a good method for aftermath.
 local connection = require(game:GetService("ReplicatedFirst").EmberClientLibrary.EmberClient.Network.Connection);
 
 local connectionHook; connectionHook = hookfunction(connection.Request, function(tab)
@@ -9,18 +10,3 @@ local connectionHook; connectionHook = hookfunction(connection.Request, function
 
     return connectionHook(tab);
 end)
-
-local backupHook; backupHook = hookfunction(Instance.new("RemoteEvent").FireServer, newcclosure(function(self, ...)
-    local args = {...};
-    local funcEnviroment = getfenv(2);
-
-    if (funcEnviroment.script.Name == "ClientGame") then 
-        local traceback = debug.traceback();
-
-        if (not string.find(traceback, "NotifyServer")) and string.find(traceback, "ReplicatedFirst.Core.ClientGame.Utilities") and (not string.find(traceback, "Utilities.Interaction")) then 
-            return;
-        end
-    end
-
-    return backupHook(self, table.unpack(args));
-end))
