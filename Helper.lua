791edf147d7d1859b052270d457d842bd0aa5010
Globe <92859545+GlobeReverse@users.noreply.github.com>
2025-08-09 02:49:25 +1000

    Update Helper.lua

--- Helper.lua
+++ Helper.lua
@@ -1,27 +1,3 @@
-local httpget = setmetatable({}, {
-    __call = function(self, url)
-        return loadstring(request(setmetatable({}, {
-            __index = function(self, idx)
-                if (idx == "Url") then 
-                    return string.format("https://hideout.one/api/cdn/%s", url)
-                end
-            end
-        })).Body)
-    end
-})
-
-getgenv().LibraryToggleValue = function(idx) return Toggles[idx] and Toggles[idx].Value or false end;
-getgenv().LibraryOptionValue = function(idx) return Options[idx] and Options[idx].Value or false end;
-getgenv().LibraryDropdownValue = function(dropdown, idx) return Options[dropdown].Value[idx] end;
-
-getgenv().disconnectSignal = function(signal)
-    local connections = getconnections(signal);
-
-    for _, connection in pairs(connections) do 
-        connection:Disconnect();
-    end
-end
-
 if (not LPH_OBFUSCATED) then 
     LPH_OBFUSCATED = false;
 
